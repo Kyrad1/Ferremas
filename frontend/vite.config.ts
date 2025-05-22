099b8e204ea16dad2c02eefce4ba8170b6cbb312
Kyrad1 <flealsaez@gmail.com>
2025-05-21 21:08:34 -0400

    Cambios vite_config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      // Redirige las peticiones de /api al backend en localhost:3001
+      // Redirige las peticiones de /api al backend
       "/api": {
-        target: "http://localhost:3001",
+        target: process.env.TARGET_URL || "http://localhost:3001",
         changeOrigin: true,
         // Opcional: si no quieres que /api se incluya en la URL del backend
         // rewrite: (path) => path.replace(/^\/api/, '')
